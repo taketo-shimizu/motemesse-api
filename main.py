0bbf37147562f538a788ffe248963ad3ab3d5521
清水 威斗 <shimizu@aiqlab.com>
2025-08-13 10:13:12 +0900

    CORS設定を更新

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ origins = [
     'http://localhost:3001',
     'http://127.0.0.1:3000',
     'http://127.0.0.1:3001',
+    'https://motemesse.vercel.app',
+    'https://motemesse-*.vercel.app',  # プレビューデプロイ用
 ]
 
 app.add_middleware(
